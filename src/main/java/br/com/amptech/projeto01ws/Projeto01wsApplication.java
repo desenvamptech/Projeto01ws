37e7e2bbe504deb64cb858e4fc1bbb8486950a2a
Andre Politti <desenv@amptech.com.br>
2018-11-12 11:46:09 -0200

    Projeto Criado

---
+++ src/main/java/br/com/amptech/projeto01ws/Projeto01wsApplication.java
@@ -0,0 +1,12 @@
+package br.com.amptech.projeto01ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto01wsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto01wsApplication.class, args);
+	}
+}
