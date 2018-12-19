38732cb38addf81c62fc2d98f79229dfaa7bac40
Andre Politti <desenv@amptech.com.br>
2018-12-19 14:16:48 -0200

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/amptech/projeto01ws/resources/ClienteResource.java
@@ -0,0 +1,28 @@
+package br.com.amptech.projeto01ws.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.amptech.projeto01ws.domain.Cliente;
+import br.com.amptech.projeto01ws.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)	
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+				
+		Cliente obj = service.Find(id);
+					
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
