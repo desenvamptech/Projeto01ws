8d10e664cbe97b23a9cec2482e2f2690d7582b38
Andre Politti <desenv@amptech.com.br>
2018-12-21 16:26:37 -0200

    PUT, DELETE, GET para cliente

--- src/main/java/br/com/amptech/projeto01ws/services/ClienteService.java
+++ src/main/java/br/com/amptech/projeto01ws/services/ClienteService.java
@@ -1,12 +1,16 @@
 package br.com.amptech.projeto01ws.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.amptech.projeto01ws.domain.Cliente;
+import br.com.amptech.projeto01ws.dto.ClienteDTO;
 import br.com.amptech.projeto01ws.repositories.ClienteRepository;
+import br.com.amptech.projeto01ws.services.exceptions.DataIntegrityException;
 import br.com.amptech.projeto01ws.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,6 +25,36 @@ public class ClienteService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {		
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());	
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		  repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui entidades relacionadas.");			
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		Cliente obj = new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		return obj;
 	}
 	
 }
