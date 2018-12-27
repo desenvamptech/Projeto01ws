70cd29317f91fc3fbdd6b1ac27ddc0b8606f541a
Andre Politti <desenv@amptech.com.br>
2018-12-27 17:20:39 -0200

    Validação customização: email não repetido na atualização de cliente

--- src/main/java/br/com/amptech/projeto01ws/dto/ClienteDTO.java
+++ src/main/java/br/com/amptech/projeto01ws/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.amptech.projeto01ws.domain.Cliente;
+import br.com.amptech.projeto01ws.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
--- src/main/java/br/com/amptech/projeto01ws/repositories/ClienteRepository.java
+++ src/main/java/br/com/amptech/projeto01ws/repositories/ClienteRepository.java
@@ -1,12 +1,15 @@
 package br.com.amptech.projeto01ws.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.amptech.projeto01ws.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
 }
