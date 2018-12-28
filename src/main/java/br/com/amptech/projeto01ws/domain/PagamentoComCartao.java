8f161c3ef9a90166cf6c27324261feef432af414
Andre Politti <desenv@amptech.com.br>
2018-12-28 14:14:08 -0200

    Inserindo o Pedido

--- src/main/java/br/com/amptech/projeto01ws/domain/PagamentoComCartao.java
+++ src/main/java/br/com/amptech/projeto01ws/domain/PagamentoComCartao.java
@@ -2,9 +2,12 @@ package br.com.amptech.projeto01ws.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import br.com.amptech.projeto01ws.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 		
 	private static final long serialVersionUID = 1L;
