093c0ae29303a510d298ee70f496691603e1c9a6
Andre Politti <desenv@amptech.com.br>
2019-01-11 16:13:31 -0200

    Autorizando endpoints para perfis especificos

--- src/main/java/br/com/amptech/projeto01ws/resources/ClienteResource.java
+++ src/main/java/br/com/amptech/projeto01ws/resources/ClienteResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,6 +60,7 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)	
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 				
@@ -67,6 +69,7 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.GET)	
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 				
